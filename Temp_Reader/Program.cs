62ffd01c8525ff678e9c9d42e34ec0e7ef6d5088
Sami <sa17022025@gmail.com>
2025-03-10 11:11:51 +0100

    Temp Converter Program

---
+++ Temp_Reader/Program.cs
@@ -0,0 +1,52 @@
+﻿using System.Security.Cryptography;
+
+namespace Temp_Reader;
+
+class Program
+{
+    static void Main(string[] args)
+    { 
+        while (true)
+        {
+            Console.WriteLine("Select Fahrenheit or Celsius (f/c)"); 
+            var input = Console.ReadLine().ToLower().Trim();    
+
+            if (input == "fahrenheit" || input =="f")  
+            {
+                Console.WriteLine("Select the amount of Farenheit you want converted to celsius");
+
+                if (int.TryParse(Console.ReadLine(), out int FarDegrees))
+                {
+                    var ConvCels = (FarDegrees - 32) * 5 / 9;
+                    Console.WriteLine("temperature is " + ConvCels + " in Celsius");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Invalid Input!!");              
+                    Console.WriteLine("Enter Valid Input!!");
+        
+                }
+            }  
+             
+            if (input == "celsius" || input == "c")
+            {
+                Console.WriteLine("Select the amount of Celsius you want converted to Fahrenheit");
+
+                if (int.TryParse(Console.ReadLine(), out int CelDegrees))
+                {
+                    var ConvFar = (CelDegrees * 9 / 5) + 32;
+                    Console.WriteLine("temperature is " + ConvFar + " in Farenheit");
+                    break;
+                }
+
+                else
+                {
+                    Console.WriteLine("Invalid Input");
+                    Console.WriteLine("Enter Valid Input!!");
+                }
+            }
+
+        }
+    }
+}
